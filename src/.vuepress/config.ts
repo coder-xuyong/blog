87f6c338657cef8349e86779999df124919ecb6a
XuYong <1299461580@qq.com>
2025-01-01 22:37:56 +0800

    去除多余的引入

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,8 +2,8 @@ import { defineUserConfig } from "vuepress";
 import { getDirname, path } from "vuepress/utils";
 import theme from "./theme.js";
 // import { searchPlugin } from "@vuepress/plugin-search";
-import { cut } from 'nodejs-jieba'
-import { slimsearchPlugin } from "@vuepress/plugin-slimsearch";
+// import { cut } from 'nodejs-jieba'
+// import { slimsearchPlugin } from "@vuepress/plugin-slimsearch";
 
 const __dirname = getDirname(import.meta.url);
 export default defineUserConfig({
