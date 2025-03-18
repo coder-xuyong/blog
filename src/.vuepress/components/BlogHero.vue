00bb67fb4b3ca1378c5d7cbcca7f5c58dc620aae
XuYong <1299461580@qq.com>
2025-03-18 21:59:30 +0800

    背景颜色黑白切换ok

--- src/.vuepress/components/BlogHero.vue
+++ src/.vuepress/components/BlogHero.vue
@@ -15,7 +15,7 @@ import BingHeroBackground from "vuepress-theme-hope/presets/BingHeroBackground.j
 <script setup lang="ts">
 import BlogHero from "vuepress-theme-hope/blog/components/BlogHero.js";
 import HitokotoBlogHero from "vuepress-theme-hope/presets/HitokotoBlogHero.js";
-import BingHeroBackground from "vuepress-theme-hope/presets/BingHeroBackground.js";
+// import BingHeroBackground from "vuepress-theme-hope/presets/BingHeroBackground.js";
 </script>
 
 <template>
